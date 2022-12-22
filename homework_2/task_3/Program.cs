17611287157e8555a5f0db5b3717ff7e04b77204
Nik <nik@nik.ru>
2022-12-23 01:25:55 +0300

    добавлен код 3 задачи

---
+++ homework_2/task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+bool CheckDay (int num)
+{
+    bool Weekend = (num >= 6);
+    return Weekend;
+     
+}
+
+Console.Write("Введите номер дня недели: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+if (numDay<=7)
+{
+    Console.WriteLine($"Выходной? -> {CheckDay(numDay)}"); 
+}
+else 
+{
+   Console.WriteLine($"Ввели недопустимое значение!");  
+}
