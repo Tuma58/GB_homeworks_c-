1e90fe1e6e8b632e902f94b2ab87adb842db53f9
Nik <nik@nik.ru>
2022-12-22 23:47:43 +0300

    добавили 1 задачку 2 домашки

---
+++ homework_2/task_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int NumberNumTwo (int InputNumber)
+{
+  int number = (InputNumber % 100)/10;
+  return number;
+}
+Console.Write("Введите 3х значное число: ");
+Console.WriteLine($"Вторая цифра равна {NumberNumTwo(Convert.ToInt32(Console.ReadLine()))}");
+Console.ReadKey();
