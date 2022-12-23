5e88d1d03c9c1d9b6d075f8b28f237a124c8160d
Nik <nik@nik.ru>
2022-12-23 14:14:29 +0300

    добавлен код 3 задачи

---
+++ homework_3/task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+double kub (double n) //возводим в куб
+{
+    double result = Math.Pow(n,3);
+    return result;
+}
+
+Console.Write("Введите число: ");
+double N = Convert.ToDouble(Console.ReadLine());
+for (double i=1;i<=N;i++)
+{
+    Console.WriteLine($"Куб числа {i} равен {kub(i)}");
+}
+Console.ReadKey();
