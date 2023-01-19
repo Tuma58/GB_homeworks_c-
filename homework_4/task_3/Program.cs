7d70c5f61d9d42cda4482beabdf6c4500cf7ba0a
Nik <nik@nik.ru>
2023-01-19 20:32:51 +0300

    по другому организовали вывод в 3 задаче

--- homework_4/task_3/Program.cs
+++ homework_4/task_3/Program.cs
@@ -1,24 +1,33 @@
 ﻿/*
-
+Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
 */
 
-static void summ(int c)
+static int[] massiv(int c)
         {
-            int p=c;
-            int sum=0;
-            while (p%10>0)
+            int[] arr =new int[c];
+            Random rnd = new Random();
+            for (int i=0;i<=c-1;i++)
             {
-                sum=sum+p%10;
-                p=p/10;
-            
+                arr[i]=rnd.Next();
+                //Console.WriteLine(arr[i]);
             }
             
-            Console.WriteLine($"сумма цифр числа {c}  равно {sum}");
+            /*for(int i=0;i<=c-1;i++)   // поробуем вывести из функции в основное тело
+            {
+                Console.Write($"{arr[i]} ");
+            } */
+            return arr;
         }
        
-Console.Write("Введите число "); // основоное тело программы
+Console.Write("Введите размерность массива "); // основоное тело программы
 int a = Convert.ToInt32(Console.ReadLine());
-
-summ(a);
+int[] mass= new int[a];
+mass=massiv(a);
+foreach(int l in mass)
+{
+    Console.Write($"{l} ");
+}
 
 Console.ReadKey();
