02a6d355eaa9dba0379f4204842e9d7f807f6768
nik <nik@nik.ru>
2023-02-15 16:43:36 +0300

    + 3 + 4 задачи

--- homework_8/task_3/Program.cs
+++ homework_8/task_3/Program.cs
@@ -13,11 +13,11 @@ static int[,] massiv(int c, int d)
             Console.WriteLine($"Генерация массива А[{c},{d}]");
             int[,] arr =new int[c,d];
             Random rnd = new Random();
-            for (int i=0;i<=c;i++)
+            for (int i=0;i<=c-1;i++)
             {
-                for (int j=0;j<=d;j++)
+                for (int j=0;j<=d-1;j++)
                     {
-                        arr[i,j]=Convert.ToInt32(rnd.Next(100));
+                        arr[i,j]=rnd.Next(100);
                     }
                 
             }         
@@ -27,23 +27,26 @@ static int[,] massiv(int c, int d)
 
 static int[,] umnogenie(int[,]a ,int[,]b)
 {
-int[,] c=new int[b.GetLength(0)-1,a.GetLength(1)-1];
+int[,] c=new int[b.GetLength(0),a.GetLength(1)];
     
 if (a.GetLength(1)==b.GetLength(0))
 {
     Console.WriteLine("Матрицы совместивы");   
 
-    for (int i=0;i<=a.GetLength(0)-1;i++)
+    for (int i=0;i<=c.GetLength(0)-1;i++)
         {   
             
-            for (int j=0;j<=a.GetLength(1)-1;j++)
+            for (int j=0;j<=c.GetLength(1)-1;j++)
                 {
                     
                     //for (int k=0;k<=b.GetLength(0);k++)
                       //  {
+
                             c[i,j]=c[i,j]+a[i,j]*b[j,i];
+                            Console.Write ($"C[{i},{j}] {c[i,j]} ");
                        // }
                 }
+                Console.WriteLine();
         }
 
 }
@@ -57,11 +60,12 @@ return c;
 
 void prit_array (int[,]arr)
 {
-    for (int i=0;i>=arr.GetLength(0)-1;i++)
+    Console.WriteLine("Вывод матрицы");
+    for (int i=0;i<=arr.GetLength(0)-1;i++)
         {
-            for (int j=0;j>=arr.GetLength(1)-1;j++)
+            for (int j=0;j<=arr.GetLength(1)-1;j++)
                 {
-                    Console.Write(arr[i,j]);
+                    Console.Write($"{arr[i,j]} ");
                 }
             Console.WriteLine();    
         }
---
+++ homework_8/task_4/Program.cs
@@ -0,0 +1,66 @@
+﻿/*
+Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+Массив размером 2 x 2 x 2
+66(0,0,0) 25(0,1,0)
+34(1,0,0) 41(1,1,0)
+27(0,0,1) 90(0,1,1)
+26(1,0,1) 55(1,1,1)
+*/
+
+static int[,,] massiv(int c, int d, int f)
+        {
+            Console.WriteLine($"Генерация массива А[{c},{d},{f}]");
+            int[,,] arr =new int[c,d,f];
+            Random rnd = new Random();
+            for (int i=0;i<=c-1;i++)
+            {
+                for (int j=0;j<=d-1;j++)
+                    {
+                        for (int k=0;k<=f-1;k++)
+                            {
+                                arr[i,j,k]=rnd.Next(100);
+                            }
+                        
+                    }
+                
+            }         
+            Console.WriteLine("Массив сгенерирован");             
+            return arr;
+        }
+
+void out_massive_3(int[,,]a)
+{
+        for (int i=0;i<=a.GetLength(0)-1;i++)
+            {
+                for (int j=0;j<=a.GetLength(1)-1;j++)
+                    {
+                        for (int k=0;k<=a.GetLength(2)-1;k++)
+                            {
+                                Console.Write ($"Элемент[{i},{j},{k}] -> {a[i,j,k]} ");
+                            }
+                        Console.WriteLine();
+                    }
+                Console.WriteLine();
+                Console.WriteLine();
+            }         
+            Console.WriteLine("Вывод массива завершен"); 
+     
+}
+
+
+/* void prit_array (int[,]arr)
+{
+    for (int i=0;i>=arr.GetLength(0)-1;i++)
+        {
+            for (int j=0;j>=arr.GetLength(1)-1;j++)
+                {
+                    Console.Write(arr[i,j]);
+                }
+            Console.WriteLine();    
+        }
+} */
+
+
+Console.Write ("!!!!!!!!!!!!!!!!!!!!!!");
+Random rd =new Random();
+out_massive_3(massiv(rd.Next(100),rd.Next(100),rd.Next(100)));
